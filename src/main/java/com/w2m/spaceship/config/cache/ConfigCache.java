1b01d76d9d5e0ad0e0a0a82a8d7f5bb63b927b6e
mfortes <mfortes@riu.com>
2024-06-07 00:05:20 -0300

    added cache config and the changed ExceptionHandler of the path

---
+++ src/main/java/com/w2m/spaceship/config/cache/ConfigCache.java
@@ -0,0 +1,34 @@
+package com.w2m.spaceship.config.cache;
+
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Configuration
+@EnableCaching
+public class CaffeineCacheSpec {
+
+    @Bean
+    public CaffeineCacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("spaceships");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    @Bean
+    public Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .weakKeys()
+                .recordStats();
+    }
+
+}
