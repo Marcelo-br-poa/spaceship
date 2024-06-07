cf7d678c4c9f5ea6d6ccbf06f8b80b52cda4018c
mfortes <mfortes@riu.com>
2024-06-07 00:07:16 -0300

    added dependency cache and refactor code the ConfigCache

--- src/main/java/com/w2m/spaceship/config/cache/ConfigCache.java
+++ src/main/java/com/w2m/spaceship/config/cache/ConfigCache.java
@@ -2,6 +2,10 @@ package com.w2m.spaceship.config.cache;
 
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -10,25 +14,30 @@ import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.TimeUnit;
 
 
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties(prefix = "cache.spaceships")
 @EnableCaching
-public class CaffeineCacheSpec {
+public class ConfigCache {
+
+    private long expireAfterWrite;
+    private int initialCapacity;
+    private int maximumSize;
 
     @Bean
-    public CaffeineCacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("spaceships");
-        cacheManager.setCaffeine(caffeineCacheBuilder());
-        return cacheManager;
-    }
-
-    @Bean
-    public Caffeine<Object, Object> caffeineCacheBuilder() {
+    public Caffeine caffeineConfig() {
         return Caffeine.newBuilder()
-                .initialCapacity(100)
-                .maximumSize(500)
-                .expireAfterAccess(10, TimeUnit.MINUTES)
-                .weakKeys()
+                .expireAfterWrite(expireAfterWrite, TimeUnit.MINUTES)
+                .initialCapacity(initialCapacity)
+                .maximumSize(maximumSize)
                 .recordStats();
     }
 
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("spaceships", "spaceshipsByName");
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
 }
