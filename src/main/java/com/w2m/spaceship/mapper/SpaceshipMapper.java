d230e042e4695083cd36c6f2d3996901aa3e77af
mfortes <mfortes@riu.com>
2024-06-11 17:08:58 -0300

    deleted unused methods

--- src/main/java/com/w2m/spaceship/mapper/SpaceshipMapper.java
+++ src/main/java/com/w2m/spaceship/mapper/SpaceshipMapper.java
@@ -5,18 +5,12 @@ import com.w2m.spaceship.dto.SpaceshipDTO;
 import com.w2m.spaceship.dto.SpaceshipRequestDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface SpaceshipMapper {
 
-    SpaceshipMapper INSTANCE = Mappers.getMapper(SpaceshipMapper.class);
-
     @Mapping(target = "id", source = "spaceship.id")
     SpaceshipDTO toDTO(Spaceship spaceship);
-
-    @Mapping(target = "id", ignore = true)
-    Spaceship toEntity(SpaceshipDTO spaceshipDTO);
 
     Spaceship toRequestEntity(SpaceshipRequestDTO spaceshipRequestDTO);
 
